ab81a7ab0d6a76ab4336e8b009fc4c402e907cf0
WietseKruk <wietsekruk@gmail.com>
2020-03-20 00:43:21 +0100

    landing page

---
+++ admin.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sending email with php</title>
+</head>
+<body>
+<form method="post" action="update_admin.php">
+    Name: <input type="text" name="name" > <br />
+    email: <input type="email" name="email" > <br />
+    Subject: <input type="text" name="subject" > <br />
+    Message: <textarea name="msg"></textarea>
+    <button type="submit" name="send_message_btn">Send</button>
+</form>
+</body>
+</html>
+
+
---
+++ login.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<head>
+    <title>Profileringsfonds landing page</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/bootstrap-4.4.1-dist/css/bootstrap.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</head>
+
+<body>
+<!-- navbar -->
+<nav class="navbar navbar-expand-sm" >
+
+    <a class="navbar-brand" href="index.php">
+        <img src="images/Logo1.png" alt="logo" style="width:150px;">
+    </a>
+
+    <!-- Links -->
+    <div id="links">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Start aanvraag</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="info.php">informatie</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="FAQ.php">FAQ</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="login.php">login</a>
+            </li>
+            <img src="images/globe.png" width="50px">
+            <li class="nav-item">
+                <a href="#" data-lang="nl">NL</a>
+            </li>/
+            <li class="nav-item active" >
+                <a href="#" data-lang="en">ENG</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<form class="jumbotron">
+    <label>Login</label>
+    <input type="text" name="username">
+
+    <input type="submit" value="log in">
+
+</form>
+
+</body>
